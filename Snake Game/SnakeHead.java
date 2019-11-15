180d5c68263da7808a7898734c24e6e1d655fc4e
Faraaz Ahmed <faraazahmed1994@gmail.com>
2019-11-15 11:48:59 -0800

    snake movement and food interaction

--- Snake Game/SnakeHead.java
+++ Snake Game/SnakeHead.java
@@ -60,4 +60,56 @@ public class SnakeHead extends Actor implements IFoodPublisher, IScoreRegister {
 
         }
     }
+
+    public void act() {
+        crawl();
+
+        // If the SnakeHead is touch the Apple then the following will be true and run
+        // the code which hides the Apple.
+        if(!GameOver.isGameOver()) {
+        if (isTouching(Food.class)) {
+            removeTouching(Food.class);
+
+            applesConsumed++;
+            if(applesConsumed%5 == 0) {
+                notifyController();
+            }
+            notifyListner();
+            speedDivision++;
+            // Greenfoot.playSound("bite.mp3");
+        }
+
+        if (isTouching(SnakeBody.class)) {
+            gameover();
+            // Greenfoot.playSound("gameOver.mp3");
+        }
+
+        if (speedDivision == 5) {
+            speedDivision = 0;
+            SPEED = SPEED - 1;
+            // Greenfoot.playSound("speedup.mp3");
+        }
+
+        if (getX() < 0) {
+            setLocation(getWorld().getWidth(), getY());
+            // Greenfoot.playSound("jump.mp3");
+        }
+
+        if (getX() > getWorld().getWidth()) {
+            setLocation(0, getY());
+            // Greenfoot.playSound("jump.mp3");
+        }
+
+        if (getY() < 0) {
+            setLocation(getX(), getWorld().getHeight());
+            // Greenfoot.playSound("jump.mp3");
+        }
+
+        if (getY() > getWorld().getHeight()) {
+            setLocation(getX(), 0);
+            // Greenfoot.playSound("jump.mp3");
+        }
+    }
+
 }
+    
