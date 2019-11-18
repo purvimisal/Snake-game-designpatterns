df845c8e9742c4b01cf3ac49c27b02dd36d930db
Purvi Misal <purvivmisal@gmail.com>
2019-11-17 22:39:57 -0800

    GameOver.java changes

--- Snake Game/GameOver.java
+++ Snake Game/GameOver.java
@@ -1,12 +1,6 @@
-/**
- * Write a description of class GameOver here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class GameOver  
 {
-    // instance variables - replace the example below with your own
     private static boolean gameOver = false;
 
     public static boolean isGameOver()
