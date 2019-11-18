07ea77c1a4e82ce3d0dbc7f123774ba498109af5
Purvi Misal <purvivmisal@gmail.com>
2019-11-17 22:32:59 -0800

    Adding Scorecard changes

--- Snake Game/ScoreCard.java
+++ Snake Game/ScoreCard.java
@@ -1,19 +1,43 @@
 import greenfoot.*;
 
-public class ScoreCard extends Actor {
-    
-    private int currentScore;
-    
-    public void updateScoreCard() {
-    
-        setImage(new GreenfootImage("Score: " + currentScore, 30, Color.BLACK, new Color(0,0,0,0)));
-    }
+public class ScoreCard extends Actor implements IScoreRegister {
 
-    public ScoreCard() {
-        updateScoreCard();
-    }
-    
-    public void act() {
-        
-    }
+	private int currentScore = 0;
+	private IScoreObserver observer;
+
+	public void updateScoreCard() {
+
+		GreenfootImage img = new GreenfootImage("Score: " + currentScore, 30, Color.BLACK, new Color(0, 0, 0, 0));
+		img.setFont(new Font("OptimusPrinceps", false, false, currentScore));
+		setImage(img);
+	}
+
+	public ScoreCard() {
+		updateScoreCard();
+	}
+
+	public void act() {
+
+	}
+
+	public void incrementScore() {
+		currentScore++;
+
+		updateScoreCard();
+		if (currentScore % 5 == 0) {
+			notifyController();
+		}
+	}
+
+	@Override
+	public void registerObserver(IScoreObserver observer) {
+		this.observer = observer;
+
+	}
+
+	@Override
+	public void notifyController() {
+
+		observer.changeState(currentScore);
+	}
 }
