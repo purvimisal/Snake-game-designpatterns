d62cb82e8810fc95b669265aa873b62e68a30f79
Shailesh Nayak <30549931+shailesh-nyk@users.noreply.github.com>
2019-11-17 22:27:50 -0800

    Update Level3.java

--- Snake Game/Level3.java
+++ Snake Game/Level3.java
@@ -1,13 +1,32 @@
 
+public class Level3 implements Ilevel {
 
-public class Level3 implements Ilevel{
+	LevelController controller;
 
-LevelController controller;
-public Level3(LevelController controller) {
+	int obstacleTime;
+	int powerUpTime;
+
+	public Level3(LevelController controller) {
+		this.powerUpTime = 7;
+		this.obstacleTime = 35;
 		this.controller = controller;
 	}
+
 	public void changeState() {
-		
+
 		controller.endGame();
 	}
+
+	@Override
+	public int getObstacleTime() {
+
+		return obstacleTime;
+	}
+
+	@Override
+	public int getPowerUpTime() {
+
+		return powerUpTime;
+	}
+
 }
