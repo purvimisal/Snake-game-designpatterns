48fb8ae0f1004d80b17b6a032ac4c9146dda78e8
Shailesh Nayak <30549931+shailesh-nyk@users.noreply.github.com>
2019-11-17 22:28:36 -0800

    Update Level1.java

--- Snake Game/Level1.java
+++ Snake Game/Level1.java
@@ -1,19 +1,34 @@
 import javax.swing.JOptionPane;
 import javax.swing.JInternalFrame;
-public class Level1 implements Ilevel{
 
-    LevelController controller;
+public class Level1 implements Ilevel {
 
-    public Level1(LevelController controller) {
-        
-        this.controller = controller;
-    }
+	LevelController controller;
+	int obstacleTime;
+	int powerUpTime;
 
-    @Override
-    public void changeState() {
-        JOptionPane.showMessageDialog(new JInternalFrame(), "Level2","Congrats!", JOptionPane.INFORMATION_MESSAGE);
-       // System.out.println("level1 to level2");
-        controller.changeLevel2();
-        
-    }
+	public Level1(LevelController controller) {
+		this.obstacleTime = 1;
+		this.powerUpTime = 100;
+		this.controller = controller;
+	}
+
+	@Override
+	public void changeState() {
+		JOptionPane.showMessageDialog(new JInternalFrame(), "Level2", "Congrats!", JOptionPane.INFORMATION_MESSAGE);
+		// System.out.println("level1 to level2");
+		controller.changeLevel2();
+	}
+
+	@Override
+	public int getObstacleTime() {
+
+		return obstacleTime;
+	}
+
+	@Override
+	public int getPowerUpTime() {
+		return powerUpTime;
+		
+	}
 }
