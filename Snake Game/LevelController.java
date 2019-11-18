f579ca69fd2f85ebaeb8454d8902c5dc54984742
Faraaz Ahmed <faraazahmed1994@gmail.com>
2019-11-17 22:28:17 -0800

    Update LevelController.java

--- Snake Game/LevelController.java
+++ Snake Game/LevelController.java
@@ -1,46 +1,59 @@
+import java.util.*;
 
-public class LevelController implements IScoreObserver {
+public class LevelController implements IScoreObserver, ILevelSubject {
 
-	Ilevel level1;
-	Ilevel level2;
-	Ilevel level3;
-	Ilevel gameOver;
-	Ilevel current;
+    Ilevel level1;
+    Ilevel level2;
+    Ilevel level3;
+    Ilevel gameOver;
+    Ilevel current;
+    Timer timer;
+    List<ILevelObserver> observers;
+    public LevelController() {
+        current = level1 = new Level1(this);
+        level2 = new Level2(this);
+        level3 = new Level3(this);
+        observers = new ArrayList<ILevelObserver>();
+        timer = new Timer();
+    }
 
-	public LevelController() {
-		
-		current = level1 = new Level1(this);
-		level2 = new Level2(this);
-		level3 = new Level3(this);
+    @Override
+    public void changeState(Integer score) {
+        current.changeState();
+    }
 
-	}
+    public String getState() {
+        return current.getClass().getName();
+    }
 
-	@Override
-	public void changeState(Integer score) {
+    public void changeLevel2() {
+        this.current = level2;
+        notifyObservers(current.getObstacleTime(),current.getPowerUpTime());
+        
+    }
 
-		current.changeState();
+    public void changeToLevel1() {
+        this.current = level1;
+        notifyObservers(current.getObstacleTime(),current.getPowerUpTime());
+    }
 
-	}
+    public void changeToLevel3() {
+         this.current = level3;
+         notifyObservers(current.getObstacleTime(),current.getPowerUpTime());
+    }
 
-	public String getState() {
-		return current.getClass().getName();
-
-	}
-
-	public void changeLevel2() {
-		this.current = level2;
-	}
-
-	public void changeToLevel1() {
-		this.current = level1;
-	}
-
-	public void changeToLevel3() {
-		this.current = level3;
-	}
-
-	public void endGame() {
-
-	}
-
+    public void endGame() {
+        
+    }
+    public void attach(ILevelObserver ob) {
+        observers.add(ob);
+    }
+    public void detach(ILevelObserver ob) {
+        observers.add(ob);
+    }
+    public void notifyObservers(int obstacleInterval, int powerUpInterval) {
+        for(ILevelObserver ob : observers) {
+            ob.update(obstacleInterval,powerUpInterval);
+        }
+    }
 }
