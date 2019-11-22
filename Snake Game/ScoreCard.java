01e6660b45846324f133f46f88d904ea16b92f1f
Shailesh Nayak <30549931+shailesh-nyk@users.noreply.github.com>
2019-11-22 10:47:43 -0800

    Score card style changed

--- Snake Game/ScoreCard.java
+++ Snake Game/ScoreCard.java
@@ -2,42 +2,51 @@ import greenfoot.*;
 
 public class ScoreCard extends Actor implements IScoreRegister {
 
-	private int currentScore = 0;
-	private IScoreObserver observer;
+    private int currentScore = 0;
+    private IScoreObserver observer;
+    private String level = "Level : 0 ";
 
-	public void updateScoreCard() {
+    public void updateScoreCard() {
+        String text = "Score: " + currentScore + "              " + level;
+        GreenfootImage textImage = new GreenfootImage(text, 24, new Color(0, 255, 128), new Color(0, 0, 0, 0));
+        GreenfootImage image = new GreenfootImage(1200, 60);
+        image.setColor(new Color(196, 196, 0));
+        image.fill();
+        image.setColor(Color.BLACK);
+        image.fillRect(0, 0, 1200, 60);
+        image.drawImage(textImage, 750, 16);
+        setImage(image);
+    }
 
-		GreenfootImage img = new GreenfootImage("Score: " + currentScore, 22, Color.BLACK, new Color(0, 0, 0, 0));
-		img.setFont(new Font("OptimusPrinceps", false, false, currentScore));
-		setImage(img);
-	}
+    public ScoreCard() {
+        updateScoreCard();
+    }
 
-	public ScoreCard() {
-		updateScoreCard();
-	}
+    public void act() {
 
-	public void act() {
+    }
 
-	}
+    public void incrementScore(String level) {
+        currentScore++;
+        StringBuilder temp = new StringBuilder(level);
+        temp.insert(temp.length()-1, " : ");
+        temp.append(" ");
+        this.level = temp.toString();
+        updateScoreCard();
+        if (currentScore % 5 == 0) {
+            notifyController();
+        }
+    }
 
-	public void incrementScore() {
-		currentScore++;
+    @Override
+    public void registerObserver(IScoreObserver observer) {
+        this.observer = observer;
 
-		updateScoreCard();
-		if (currentScore % 5 == 0) {
-			notifyController();
-		}
-	}
+    }
 
-	@Override
-	public void registerObserver(IScoreObserver observer) {
-		this.observer = observer;
+    @Override
+    public void notifyController() {
 
-	}
-
-	@Override
-	public void notifyController() {
-
-		observer.changeState(currentScore);
-	}
+        observer.changeState(currentScore);
+    }
 }
