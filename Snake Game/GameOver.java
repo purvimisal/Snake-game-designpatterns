9f3bc07b85560e935430adfafb892cbb1d22c33d
Purvi Misal <purvivmisal@gmail.com>
2019-11-22 12:24:51 -0800

    Added writescores function

--- Snake Game/GameOver.java
+++ Snake Game/GameOver.java
@@ -1,6 +1,14 @@
-
+import java.io.*;
+import java.util.*;
+/**
+ * Write a description of class GameOver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class GameOver  
 {
+    // instance variables - replace the example below with your own
     private static boolean gameOver = false;
 
     public static boolean isGameOver()
@@ -18,4 +26,22 @@ public class GameOver
     {
         gameOver = false;
     }
+    
+    public static void writeScores(String name, int score) {
+    try {
+        File file = new File("scores.txt");
+        FileWriter fr;
+        BufferedWriter br;
+        fr = new FileWriter(file, true);
+        br = new BufferedWriter(fr);
+        br.write(""+name+" "+score);
+        br.newLine();
+
+        br.close();
+        fr.close();
+    }
+    catch(Exception e) { }
+}    
+    
+    
 }
