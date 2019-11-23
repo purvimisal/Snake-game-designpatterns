b6c5887040b51bb1d35a9422a99cea433b1fe513
anuragpatro92 <anuragpatro92@gmail.com>
2019-11-22 17:48:19 -0800

    Update Alaska.java

--- Snake Game/Alaska.java
+++ Snake Game/Alaska.java
@@ -1,12 +1,15 @@
-
+import java.util.*;
 public class Alaska implements ICities {
 
 	private String image;
 	private String name;
+	private List<String> foods;
 
 	public Alaska(String image, String name) {
 		this.image = image;
 		this.name = name;
+			foods = new ArrayList();
+			foods.add("fries");
 	}
 
 	@Override
@@ -19,6 +22,11 @@ public class Alaska implements ICities {
 	public String getImage() {
 		// TODO Auto-generated method stub
 		return image;
+	}
+	
+	@Override
+	public List getFood(){
+		return foods;
 	}
 
 }
